77a71500e6555c1b6d1b3bc65283023153bac154
Andre <Patricreyneke@gmail.com>
2023-02-16 13:35:13 -0800

    6fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,14 @@
  * Description:printf funt
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
+char(a);
+int(b);
+long int(c);
+long long int(d);
+float(e);
 
 printf("Size of a char: %ul byte(s)\n", sizeof(char));
 printf("Size of an int: %ul byte(s)\n", sizeof(int));
