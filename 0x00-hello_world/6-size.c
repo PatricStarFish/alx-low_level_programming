2b6b72763b1cca95ad94e18992149e75a388ef7d
Andre <Patricreyneke@gmail.com>
2023-02-16 13:36:51 -0800

    6fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ long int(c);
 long long int(d);
 float(e);
 
-printf("Size of a char: %ul byte(s)\n", sizeof(char));
-printf("Size of an int: %ul byte(s)\n", sizeof(int));
-printf("Size of a long int: %ul byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %ul byte(s)\n", sizeof(long long int));
-printf("Size of a float: %ul byte(s)\n", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 return (0);
 }
