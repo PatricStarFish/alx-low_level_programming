4af18d8191852ed9275fdfe2616a8d2ff3f907a5
Andre <Patricreyneke@gmail.com>
2023-02-16 13:23:37 -0800

    5 fix

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - Description this is des for printF
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
