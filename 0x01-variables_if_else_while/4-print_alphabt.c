68fe60a82ec04db86fba2d81f9abde3eb619f2c4
Andre <Patricreyneke@gmail.com>
2023-02-19 10:31:52 -0800

    4 and non c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description:printf funt
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+c = 97;
+
+while (c < 123)
+{
+if (c == 101 || c == 113)
+{
+}
+else
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
