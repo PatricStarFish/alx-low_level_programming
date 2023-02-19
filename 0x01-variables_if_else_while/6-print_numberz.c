608275ffcb0319444c2672fa3ba74d5855882736
Andre <Patricreyneke@gmail.com>
2023-02-19 10:37:15 -0800

    file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description:printf funt
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+c = 48;
+while (c < 58)
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
