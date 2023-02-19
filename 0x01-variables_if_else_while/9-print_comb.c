5dfc02b5f085e5b7e97a063b2a499c45f20f65f6
Andre <Patricreyneke@gmail.com>
2023-02-19 10:52:44 -0800

    final file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description:printf funt
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+c = 48;
+while (c < 58)
+{
+putchar(c);
+putchar(44);
+putchar(32);
+c++;
+}
+putchar('\n');
+return (0);
+}
