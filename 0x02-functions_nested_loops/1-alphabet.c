ecd75deeac2a4b643f50f7f634e730577b9bbdf1
Andre <Patricreyneke@gmail.com>
2023-02-21 09:44:31 -0800

    file 2 with header

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main1.h"
+/**
+ * main - Entry point
+ * Description:printf funt
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+print_alphabet();
+return (0);
+}
--- 0x02-functions_nested_loops/alf.c
+++ 0x02-functions_nested_loops/alf.c
@@ -9,24 +9,12 @@
 int main(void)
 {
 int c;
-c = 95;
-putchar(c);
-c = 112;
-putchar(c);
-c = 117;
-putchar(c);
-c = 116;
-putchar(c);
-c = 99;
-putchar(c);
-c = 104;
-putchar(c);
 c = 97;
+while (c < 122)
+{
 putchar(c);
-c = 114;
-putchar(c);
- 
- 
- putchar('\n');
+c++;
+}
+putchar('\n');
 return (0);
 }
