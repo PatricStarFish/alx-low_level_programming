b5242f729095663e76a77cfa88e4d325b74a0f68
Andre <Patricreyneke@gmail.com>
2023-02-21 08:56:51 -0800

    folder 0x02

---
+++ 0x02-functions_nested_loops/mai.c
@@ -0,0 +1,18 @@
+include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description:printf funt
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+c = 95;
+putchar(c);
+c = 112;
+putchar(c);
+putchar('\n');
+return (0);
+}
