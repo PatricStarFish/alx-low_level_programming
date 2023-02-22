7dd19386f2b7e3f155e24698098b313d1ae29f25
Andre <Patricreyneke@gmail.com>
2023-02-22 03:16:30 -0800

    betty not rite

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _islower - Start of Script
+ * @d;comes from main
+ * Description:runs main from main.h
+ * Return: Always 0 (Success)
+ */
+int _islower(char d)
+{
+if (islower(d) == 0)
+{
+return (0);
+}
+else
+return (1);
+}
