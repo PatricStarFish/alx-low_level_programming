52dbcd3c1a5c50eaaf171718378599a6ef7a2e30
Andre <Patricreyneke@gmail.com>
2023-02-22 02:05:00 -0800

    fix the betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - Start of Script
+ * print_alphabet_x10 - Start of Script
  * Description:runs main from main.h
  * Return: Always 0 (Success)
  */
@@ -8,7 +8,7 @@ int print_alphabet_x10(void)
 {
 int d;
 d = 0;
-while(d < 11)
+while (d < 11)
 {
 int c;
 c = 97;
@@ -17,7 +17,7 @@ while (c < 123)
 _putchar(c);
 c++;
 }
-d++; 
+d++;
 _putchar('\n');
 return (0);
-}
+ }}
